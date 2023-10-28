18da3758a341c6bff8b0b4dace5072b55213694a
GasinAn <Gasin185@163.com>
2023-10-28 17:31:24 +0800

    succeed to float64, but with warning

--- bitround.c
+++ bitround.c
@@ -1,49 +1,49 @@
 #include <math.h>
 #include <stdio.h>
 
-#define HEX_00400000 4194304
-#define HEX_7F800000 2139095040
-#define HEX_80000000 -2147483648
-#define HEX_FF800000 -8388608
+#define HEX_00080s 2251799813685248L
+#define HEX_7FF00s 9218868437227405312L
+#define HEX_80000s -9223372036854775808L
+#define HEX_FFF00s -4503599627370496L
 
-typedef int int32;
-typedef unsigned int uint32;
-typedef float float32;
+typedef long int64;
+typedef unsigned long uint64;
+typedef double float64;
 
-float32 bitround(float32 r, float32 d){
-    uint32* p_r = (uint32*) &r;
-    uint32* p_d = (uint32*) &d;
+float64 bitround(float64 r, float64 d){
+    uint64* p_r = (uint64*) &r;
+    uint64* p_d = (uint64*) &d;
 
-    int32 E_r = *p_r & HEX_7F800000;
-    int32 E_d = *p_d & HEX_7F800000;
-    int32 dE = (E_r - E_d) >> 23;
+    int64 E_r = *p_r & HEX_7FF00s;
+    int64 E_d = *p_d & HEX_7FF00s;
+    int64 dE = (E_r - E_d) >> 52;
 
-    uint32 output = \
-        (dE > -1) * ((*p_r + (HEX_00400000 >> dE)) & (HEX_FF800000 >> dE))\
+    uint64 output = \
+        (dE > -1) * ((*p_r + (HEX_00080s >> dE)) & (HEX_FFF00s >> dE))\
                   + \
-        (dE == -1) * ((*p_r & HEX_80000000) | E_d);
+        (dE == -1) * ((*p_r & HEX_80000s) | E_d);
 
-    return *((float32*) &output);
+    return *((float64*) &output);
 }
 
 int test_const(){
-    uint32 c;
-    c = 0x00400000;
-    printf("%d\n", *((int32*) &c));
-    c = 0x7F800000;
-    printf("%d\n", *((int32*) &c));
-    c = 0x80000000;
-    printf("%d\n", *((int32*) &c));
-    c = 0xFF800000;
-    printf("%d\n", *((int32*) &c));
+    uint64 c;
+    c = 0x0008000000000000;
+    printf("%f\n", (float64) *((int64*) &c));
+    c = 0x7FF0000000000000;
+    printf("%f\n", (float64) *((int64*) &c));
+    c = 0x8000000000000000;
+    printf("%f\n", (float64) *((int64*) &c));
+    c = 0xFFF0000000000000;
+    printf("%f\n", (float64) *((int64*) &c));
     return 0;
 }
 
 int test(){
     test_const();
     printf("\n");
-    float32 r_list[4] = {1.875, 1.125, -1.875, -1.125};
-    float32 d_list[7] = {0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0};
+    float64 r_list[4] = {1.875, 1.125, -1.875, -1.125};
+    float64 d_list[7] = {0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0};
     for (int i=0; i<4; i++)
     {
         for (int j=0; j<7; j++)
