b2d6f519c09064e503c0ed0920a39e688b5bd3af
GasinAn <Gasin185@163.com>
2023-11-01 20:14:28 +0800

    try config setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
+from setuptools import Extension
 from setuptools import setup
 
 setup(
-
+    name="bitround",
+    version="0.3.0",
+    author="GasinAn",
+    author_email="Gasin185@163.com",
+    maintainer="GasinAn",
+    maintainer_email="Gasin185@163.com",
+    url="https://github.com/GasinAn/bitround",
+    license="MIT",
+    packages=["bitround"],
+    package_dir={"bitround": "bitround"},
+    ext_modules=[
+        Extension(
+            name="bitround.core",
+            sources=["bitround/core.c"],
+        )
+    ],
+    py_modules=["bitround.test"],
+    python_requires=">=3.0",
+    install_requires=[],
 )
