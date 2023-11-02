86236a1d4d1300aa43aade4fb473d4aeae895eaa
GasinAn <Gasin185@163.com>
2023-11-02 20:35:31 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy
 
 setuptools.setup(
     name='bitround',
-    version='0.3.0',
+    version='0.3.1',
     description='"Bit round" for fast lossy compression',
     author='GasinAn',
     author_email='Gasin185@163.com',
