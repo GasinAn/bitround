72114bf9e053605b3929221be3e68f79cec53306
GasinAn <Gasin185@163.com>
2023-11-03 17:17:16 +0800

    create func _bitround

--- bitround/_test.py
+++ bitround/_test.py
@@ -1,2 +1,10 @@
+from numpy import *
+
+@vectorize
+def _bitround(a, d):
+    if (a==0) or isinf(a) or isnan(a): return a
+    n = floor(log2(d))
+    return sign(a)*floor((abs(a)+exp2(n))/exp2(n+1))*exp2(n+1)
+
 def test():
     print('func test')
