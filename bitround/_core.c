776ffbf360a3b217cceb458e5789ef929f127c8a
GasinAn <Gasin185@163.com>
2023-11-04 16:20:52 +0800

    rename npy_double to npy_float64

--- bitround/_core.c
+++ bitround/_core.c
@@ -5,7 +5,7 @@
 #include "numpy/ufuncobject.h"
 
 
-/*** npy_double_bitround ***/
+/*** npy_float64_bitround ***/
 
 const npy_int64 ONE = 1;
 const npy_int64 HEX_00100s = ONE << 52;
@@ -14,7 +14,7 @@ const npy_int64 HEX_80000s = ONE << (64 - 1);
 const npy_int64 HEX_FFE00s = ((ONE << 11) - 1) << (52 + 1);
 const npy_int64 HEX_FFF00s = ((ONE << (1 + 11)) - 1) << 52;
 
-inline npy_double npy_double_bitround(npy_double a, npy_double d){
+inline npy_float64 npy_float64_bitround(npy_float64 a, npy_float64 d){
     npy_uint64* p_a = (npy_uint64*) &a;
     npy_uint64* p_d = (npy_uint64*) &d;
 
@@ -29,7 +29,7 @@ inline npy_double npy_double_bitround(npy_double a, npy_double d){
     output = ((E_a != HEX_7FF00s) && (dE < 64))  * output
            + ((E_a == HEX_7FF00s) || (dE >= 64)) * *p_a;
 
-    return *((npy_double*) &output);
+    return *((npy_float64*) &output);
 }
 
 
@@ -37,10 +37,10 @@ inline npy_double npy_double_bitround(npy_double a, npy_double d){
 
 /* The loop definition must precede the PyMODINIT_FUNC. */
 
-static void u_npy_double_bitround(char **args,
-                                  const npy_intp *dimensions,
-                                  const npy_intp *steps,
-                                  void *data)
+static void u_npy_float64_bitround(char **args,
+                                   const npy_intp *dimensions,
+                                   const npy_intp *steps,
+                                   void *data)
 {
     npy_intp i;
     npy_intp n = dimensions[0];
@@ -51,8 +51,8 @@ static void u_npy_double_bitround(char **args,
 
     for (i = 0; i < n; i++) {
         /*BEGIN main ufunc computation*/
-        *((npy_double *)out1) = npy_double_bitround(
-            *(npy_double *)in1, *(npy_double *)in2
+        *((npy_float64 *)out1) = npy_float64_bitround(
+            *(npy_float64 *)in1, *(npy_float64 *)in2
         );
         /*END main ufunc computation*/
 
@@ -64,7 +64,7 @@ static void u_npy_double_bitround(char **args,
 
 /* This a pointer to the above function. */
 
-PyUFuncGenericFunction funcs_bitround[1] = {&u_npy_double_bitround};
+PyUFuncGenericFunction funcs_bitround[1] = {&u_npy_float64_bitround};
 
 /* These are the input and output dtypes of bitround. */
 
