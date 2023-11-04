8458f5f5eb9ee621e8499fa40d574f386c1963bc
GasinAn <Gasin185@163.com>
2023-11-04 15:40:09 +0800

    simplify _bitround

--- bitround/_test.py
+++ bitround/_test.py
@@ -3,9 +3,11 @@ from numpy import *
 
 @vectorize
 def _bitround(a, d):
-    if (a==0) or isinf(a) or isnan(a): return a
-    exp2_n = exp2(ceil(log2(d)))
-    return sign(a)*round(abs(a)/exp2_n)*exp2_n
+    if isfinite(a):
+        exp2_n = exp2(ceil(log2(d)))
+        return sign(a)*round(abs(a)/exp2_n)*exp2_n
+    else:
+        return a
 
 def test():
     print('func test')
