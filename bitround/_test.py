ed7f7804f6207c2a57e150ac2861deb3852e73b7
GasinAn <Gasin185@163.com>
2023-11-06 11:54:01 +0800

    fix mistake in test._bitround

--- bitround/_test.py
+++ bitround/_test.py
@@ -4,7 +4,7 @@ from numpy import *
 @vectorize
 def _bitround(a, d):
     if isfinite(a):
-        exp2_n = exp2(ceil(log2(d)))
+        exp2_n = exp2(floor(log2(d))+1)
         return sign(a)*round(abs(a)/exp2_n)*exp2_n
     else:
         return a
